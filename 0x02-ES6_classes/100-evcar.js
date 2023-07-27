eb0361c9231fc9fd799f68f61901e53050ba8d93
Budabos <opiyocaroline20@gmail.com>
2023-07-27 20:17:11 +0000

    0x02-ES6_classes

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -10,16 +10,4 @@ export default class EVCar extends Car {
   cloneCar() {
     return new Car();
   }
-}/* eslint-disable class-methods-use-this */
-import Car from './10-car';
-
-export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this._range = range;
-  }
-
-  cloneCar() {
-    return new Car();
-  }
 }
