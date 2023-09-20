640d15707545a5109b07c430f85c1dacbe8fd621
caroline opiyo <opiyocaroline20@gmail.com>
2023-09-20 04:32:28 +0300

    0x05-Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-main.js
@@ -0,0 +1,3 @@
+t displayMessage = require('./0-console');
+
+displayMessage("Hello NodeJS!");
---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,7 @@
+ass AppController {
+    static getHomepage(request, response) {
+      response.send(200, 'Hello Holberton School!');
+    }
+  }
+  
+  export default AppController;
